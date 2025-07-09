29444f749c33344b6ab37efefa08f6c038336722
nika767 <nikolozzarnadze37@gmail.com>
2025-07-09 13:59:32 +0400

    1

--- level 03/homework/homework1.py
+++ level 03/homework/homework1.py
@@ -1,1 +1,54 @@
-111
+#1) snacke_case მათემატიკური მოქმედებები
+number_1=15
+number_2=45
+sum_of_numbers=number_1+number_2
+print(sum_of_numbers)
+diference_of_number=number_2-number_1
+print(diference_of_number)
+division_of_numbers=number_2/number_1
+print(division_of_numbers)
+multiplied_of_numbers=number_1*number_2
+print(multiplied_of_numbers)
+
+#2)  ცვლადის აბდეითი(განახლება)
+
+name="nika"
+name="teimuraza"
+name="bondoviChi"
+name="jamalichi"
+name="hamleta"
+print(name)
+
+#3)  python როგორც case__ensityve ენა 
+Name="nika"
+nAme="giorgi"
+naMe="luka"
+namE="jorji"
+NAME="solomoni"
+print(Name , nAme , naMe , namE , NAME)
+
+#4)შეცდომების გასწორება
+
+name = "giorgi"
+username = "bubunauri"
+user_name =" goga"
+user_name= "axalaia"
+
+#5) სტრინგების გამოტანა 
+
+N1="me"
+N2="vcxovrob"
+N3="vazisubnis"                  #სტრინგების ერთმანეთზე დამატებას (შეერთებას) კონკატინაცია ეწოდება
+N4="teqvsmet"
+N5="nomershi"
+print(N1 , N2 , N3 , N4 , N5)
+
+#6) სტრინგისა და ინტეჯერის ნამრავლი
+
+name="nika"
+quantity=10
+print(name*quantity)
+
+#7)   კომენტარის სახით ახსნა
+
+#ჩვენ სტრინგზე და ინტეჯერზე არ შეგვიძლია (/ , + , -) მოქმედებების განხორციელება და ეს ყველაფერი ჩვეენ ტერმინალში error_ს მოგვცემს
