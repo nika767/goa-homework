2ea2f085ac177990542774e6a8cdb06797c642ec
nika767 <nikolozzarnadze37@gmail.com>
2025-07-09 13:26:16 +0400

    1

--- level 03/classwork/cw1.py
+++ level 03/classwork/cw1.py
@@ -1,1 +1,51 @@
-111
+# first classwork
+
+
+name="grigolia"
+name="jamalichi"
+print(name)
+
+
+
+#2)
+
+
+#2 second classwork
+Name=1
+nAme=2
+naMe=3
+namE=4
+NAME=5
+
+#3) 
+
+
+# third classwork
+
+#string
+teachers_name="eka"
+fathers_name="giorgi"
+mothers_name="sofo"
+brothers_name="andria"
+
+#integer
+my_age=16
+fathers_age=36
+mothers_age=34
+brothers_age=14
+
+#float
+teachers_height=1.87
+fathers_height=1.98
+mothers_height=1.78
+brothers_height=1.65
+print(teachers_name , fathers_name , mothers_name , brothers_name , my_age , fathers_age , mothers_age , brothers_age , teachers_height , fathers_height , mothers_height , brothers_height)
+
+
+#4) 
+
+#fourth classwork
+user_name = "goga"
+user_name = "chalauri"
+first_number = 200
+second_number = 123
