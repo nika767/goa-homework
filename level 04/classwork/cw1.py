f5d481a06d7d0f44767586c58cad44c4da074098
nika767 <nikolozzarnadze37@gmail.com>
2025-07-13 15:54:52 +0400

    1

---
+++ level 04/classwork/cw1.py
@@ -0,0 +1,21 @@
+name = input("შეიყვანე შენი სახელი: ")
+print("შენი სახელი არის:", name)
+
+#2
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+sum = num1 + num2
+print("ორი რიცხვის ჯამი არის:", sum)
+#3
+age = input("შეიყვანე შენი ასაკი: ")
+print("შენი ასაკის ტიპია:", type(age))
+#4
+a = 10           # მთელი რიცხვი (int)
+b = 3.14         # ათწილადი რიცხვი (float)
+c = "გამარჯობა"  # ტექსტი (str)
+d = True         # ლოგიკური მნიშვნელობა (bool)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
